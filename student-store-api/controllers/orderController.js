8baf73fb45f293f7f1c9c8d9c651902531ec53c8
ohziiilov3 <klbaskerville0520@gmail.com>
2025-05-15 11:24:00 -0700

    Milestone 3: Order Model

--- student-store-api/controllers/orderController.js
+++ student-store-api/controllers/orderController.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 
 
-
+// create order
 
 const createOrder = async (req,res)=>{
   const { userId, totalPrice, status} = req.body;
@@ -38,8 +38,54 @@ const getAllOrders = async (req, res) => {
 };
 
 
+// get order by id 
+const getOrderById = async (req,res)=>{
+   const { id } = req.params;
+  try {
+    const order = await prisma.order.findUnique({
+      where: { orderId: parseInt(id) },
+    });
+
+    if (!order) return res.status(404).json({ error: "Order not found." });
+    res.json(order);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Update Order
+const updateOrder = async (req, res) => {
+    const { id } = req.params;
+    const { userId, totalPrice, status } = req.body;
+    try {
+        const order = await prisma.order.update({
+            where: { orderId: parseInt(id) },
+            data: { userId, totalPrice, status },
+        });
+        res.json(order);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+// Delete Order
+const deleteOrder = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const order = await prisma.order.delete({
+            where: { orderId: parseInt(id) },
+        });
+        res.json({ message: 'Order deleted', order });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
 
 module.exports ={
     createOrder,
-    getAllOrders
+    getAllOrders,
+    getOrderById,
+    updateOrder,
+    deleteOrder
 }
--- student-store-api/routes/orderRoutes.js
+++ student-store-api/routes/orderRoutes.js
@@ -5,7 +5,8 @@ const orderController = require("../controllers/orderController");
 
 router.post("/", orderController.createOrder);
 router.get("/", orderController.getAllOrders);
-
-
+router.get('/:id', orderController.getOrderById);
+router.put("/:id", orderController.updateOrder);
+router.delete("/:id", orderController.deleteOrder);
 
 module.exports = router;
